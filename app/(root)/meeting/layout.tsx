dab69f234cffa7a502d4f1bdde69390c9aacec50
pankaj-404 <>
2024-05-18 20:06:01 +0530

    wave development
    Next JS web app for video calls.

---
+++ app/(root)/meeting/layout.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from 'react';
+import { Metadata } from 'next';
+
+import StreamVideoProvider from '@/providers/StreamClientProvider';
+
+export const metadata: Metadata = {
+  title: "WAVE",
+  description: "Video calling App",
+  icons: {
+    icon: "/icons/logo.svg",
+  },
+};
+
+const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
+  return (
+    <main>
+      <StreamVideoProvider>{children}</StreamVideoProvider>
+    </main>
+  );
+};
+
+export default RootLayout;
